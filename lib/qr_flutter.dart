ccf251e74d5fe9c0f5c408f0b09b50028dac281c
Steffen Haak <haak@aucentiq.com>
2022-01-13 10:46:38 +0100

    bump to qr 3.0.0

--- lib/qr_flutter.dart
+++ lib/qr_flutter.dart
@@ -4,8 +4,6 @@
  * See LICENSE for distribution and usage details.
  */
 
-export 'package:qr/qr.dart';
-
 export 'src/errors.dart';
 export 'src/qr_image.dart';
 export 'src/qr_painter.dart';
--- lib/src/qr_painter.dart
+++ lib/src/qr_painter.dart
@@ -70,6 +70,7 @@ class QrPainter extends CustomPainter {
   })  : _qr = qr,
         version = qr.typeNumber,
         errorCorrectionLevel = qr.errorCorrectLevel {
+    if (_qr != null) _qrImage = QrImage(_qr!);
     _calcVersion = version;
     _initPaints();
   }
@@ -108,6 +109,8 @@ class QrPainter extends CustomPainter {
   /// The base QR code data
   QrCode? _qr;
 
+  QrImage? _qrImage;
+
   /// This is the version (after calculating) that we will use if the user has
   /// requested the 'auto' version.
   late final int _calcVersion;
@@ -132,6 +135,7 @@ class QrPainter extends CustomPainter {
       throw validationResult.error!;
     }
     _qr = validationResult.qrCode;
+    if (_qr != null) _qrImage = QrImage(_qr!);
     _calcVersion = _qr!.typeNumber;
     _initPaints();
   }
@@ -210,7 +214,7 @@ class QrPainter extends CustomPainter {
       for (var y = 0; y < _qr!.moduleCount; y++) {
         // draw the finder patterns independently
         if (_isFinderPatternPosition(x, y)) continue;
-        final paint = _qr!.isDark(y, x) ? pixelPaint : emptyPixelPaint;
+        final paint = _qrImage!.isDark(y, x) ? pixelPaint : emptyPixelPaint;
         if (paint == null) continue;
         // paint a pixel
         left = paintMetrics.inset + (x * (paintMetrics.pixelSize + gap));
@@ -258,12 +262,12 @@ class QrPainter extends CustomPainter {
 
   bool _hasAdjacentVerticalPixel(int x, int y, int moduleCount) {
     if (y + 1 >= moduleCount) return false;
-    return _qr!.isDark(y + 1, x);
+    return _qrImage!.isDark(y + 1, x);
   }
 
   bool _hasAdjacentHorizontalPixel(int x, int y, int moduleCount) {
     if (x + 1 >= moduleCount) return false;
-    return _qr!.isDark(y, x + 1);
+    return _qrImage!.isDark(y, x + 1);
   }
 
   bool _isFinderPatternPosition(int x, int y) {
